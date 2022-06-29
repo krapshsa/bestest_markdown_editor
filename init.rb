94c0f066c8880b3f014b57da1f8c109983dd0941
kevin_chiu <kevin_chiu@openfind.com.tw>
2022-06-29 10:45:21 +0800

    Update url.

--- init.rb
+++ init.rb
@@ -5,6 +5,6 @@ Redmine::Plugin.register :bestest_markdown_editor do
   author 'Martin Blom'
   description "This is an awesome Markdown editor for Redmine. It's great. It's the bestest."
   version '1.2.0'
-  url 'https://github.com/LeviticusMB/bestest_markdown_editor'
+  url 'https://github.com/krapshsa/bestest_markdown_editor'
   author_url 'https://github.com/LeviticusMB'
 end
